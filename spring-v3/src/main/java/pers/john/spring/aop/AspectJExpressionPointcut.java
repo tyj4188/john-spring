b57107ce18ddf1623f4b8650bc26441cd6450c3d
tongyongjian <tongyongjian@olayc.cn>
2019-06-19 20:28:12 +0800

    新增：V3 版本，AOP 功能开发

--- spring-v3/src/main/java/pers/john/spring/aop/AspectJExpressionPointcut.java
+++ spring-v3/src/main/java/pers/john/spring/aop/AspectJExpressionPointcut.java
@@ -1,16 +1,41 @@
 package pers.john.spring.aop;
 
+import org.aspectj.weaver.tools.PointcutExpression;
+import org.aspectj.weaver.tools.PointcutParser;
+import org.aspectj.weaver.tools.ShadowMatch;
+
 import java.lang.reflect.Method;
+import java.util.Objects;
 
 /**
  * AspectJ 表达式的切点实现
  */
 public class AspectJExpressionPointcut implements Pointcut {
+
+    private String expression;
+
+    private PointcutParser pointcutParser;
+
+    private PointcutExpression pointcutExpression;
+
+    public AspectJExpressionPointcut(String expression) {
+        Objects.requireNonNull(expression, "切面表达式不能为 NULL");
+        this.expression = expression;
+        this.pointcutParser = PointcutParser
+            .getPointcutParserSupportingAllPrimitivesAndUsingContextClassloaderForResolution();
+        this.pointcutExpression = pointcutParser.parsePointcutExpression(expression);
+    }
+
     public boolean matchClass(Class<?> beanClass) {
-        return false;
+        return pointcutExpression.couldMatchJoinPointsInType(beanClass);
     }
 
     public boolean matchMethod(Method method, Class<?> beanClass) {
-        return false;
+        ShadowMatch shadowMatch = pointcutExpression.matchesMethodExecution(method);
+        return shadowMatch.alwaysMatches();
+    }
+
+    public String getExpression() {
+        return expression;
     }
 }
